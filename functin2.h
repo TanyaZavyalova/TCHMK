1971145b99d8beba82ee0e81afa767b5974923c7
TanyaZavyalova <arfa037@gmail.com>
2015-06-10 16:03:08 +0800

    Create functin2.h

---
+++ functin2.h
@@ -0,0 +1,34 @@
+extern "C"
+{
+#include "function.h"
+}
+
+class veryLong
+{
+public:
+	unsigned char *number;
+	int length;
+	bool binary;
+	unsigned char sign;
+	veryLong();
+	veryLong(const veryLong &);
+	~veryLong();
+	int rd(char*);
+	int rdb(char*);
+	int out(char*);
+	veryLong operator+(veryLong );
+	veryLong operator-(veryLong );
+    	veryLong operator*(veryLong );
+	veryLong operator/(veryLong );
+	veryLong operator%(veryLong );
+	veryLong operator^(veryLong );
+	veryLong operator=(veryLong );
+	veryLong operator=(const char*);
+	bool operator==(veryLong );
+	bool operator<(veryLong );
+	bool operator>(veryLong );
+	bool operator<=(veryLong );
+	bool operator>=(veryLong );
+private:
+	int strlenn(unsigned char**);
+};
